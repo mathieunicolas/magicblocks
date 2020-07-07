9f4e943edfce67a7c550248cff70977f63192892
Mathieu NICOLAS <mathieu.nicolas@me.com>
2020-07-07 04:26:27 +0200

    viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,12 @@
-import bodyParser from 'body-parser'
+import bodyParser from "body-parser";
 export default {
-    head: {
-        title: 'The next INT Titans Magic Block !'
-    },
-    modules: [
-        '@nuxtjs/axios'
-    ],
-    serverMiddleware: [
-        bodyParser.json(),
-        '~/api'
-    ]
-}
+  head: {
+    title: "The next INT Titans Magic Block !"
+  },
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" }
+  ],
+  modules: ["@nuxtjs/axios"],
+  serverMiddleware: [bodyParser.json(), "~/api"]
+};
